b6fab02385abcca1aef81ddc25961bba0063fb37
mohd-musheer <musheerayan@gmail.com>
2026-01-30 14:55:51 +0530

    predict v2

--- app/predict.py
+++ app/predict.py
@@ -1,32 +1,8 @@
 import joblib
 import pandas as pd
 
-loaded_model = joblib.load("../model/ev_efficiency_classifier.pkl")
+loaded_model = joblib.load("model/ev_efficiency_classifier.pkl")
 
-
-new_ev = pd.DataFrame([{
-    "manufacturer": "Tesla",
-    "model": "Model 3",
-    "type": "Sedan",
-    "drive_type": "RWD",
-    "fuel_type": "Electric",
-    "color": "White",
-    "fast_charging": 1,
-    "country": "USA",
-    "city": "San Francisco",
-
-    "battery_kwh": 60,
-    "range_km": 450,
-    "charging_time_hr": 1.2,
-    "release_year": 2023,
-    "seats": 5,
-    "acceleration_0_100_kmph": 5.8,
-    "top_speed_kmph": 225,
-    "warranty_years": 8,
-    "cargo_space_liters": 425,
-    "safety_rating": 5
-}])
-import pandas as pd
 
 new_ev = pd.DataFrame([{
     "manufacturer": "Tesla",
